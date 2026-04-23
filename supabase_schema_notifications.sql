bb51543cc5def4e19115fcb5203c025cc248dd25
Rense1 <rensei.kawakami@gmail.com>
2026-04-24 00:30:39 +1200

    cleanup: remove schema fragments from repo

--- supabase_schema_notifications.sql
+++
@@ -1,4 +0,0 @@
--- Add notification columns to tasks table
-ALTER TABLE tasks
-  ADD COLUMN IF NOT EXISTS notification_enabled BOOLEAN DEFAULT FALSE,
-  ADD COLUMN IF NOT EXISTS notification_times JSONB DEFAULT NULL;
